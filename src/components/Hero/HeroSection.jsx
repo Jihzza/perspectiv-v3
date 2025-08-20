f9beada4d6a6a942b2a50362fb4409898efb7f19
Jihzza <rafael.ravalian@gmail.com>
2025-08-20 03:50:06 +0200

    Latest Working Version

--- src/components/Hero/HeroSection.jsx
+++ src/components/Hero/HeroSection.jsx
@@ -15,7 +15,7 @@ export default function HeroSection() {
         {/* Logo */}
         <img src={PerspectivLogo} alt="Perspectiv Logo" className="w-45 h-45"/>
 
-        <p className="text-white text-lg text-center pb-8 font-bold">We help you integrate AI into your business!</p>
+        <p className="text-white text-xl text-center pb-8 font-bold">We help you integrate AI into your business!</p>
         {/* Service boxes */}
         <div className="flex justify-center items-center gap-2 w-full">
           {services.map(({ icon, label }) => (
